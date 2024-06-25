dc3b95b9cb51b38cdb713a0dfcc33dec928057e9
Javan <javan_dhein@outlook.com>
2024-06-25 12:01:37 -0300

    fix: Refatorado o codigo para conexão funcionar

--- conexao.php
+++ conexao.php
@@ -4,6 +4,7 @@ $username = "root";
 $password = "2715";
 $dbname = "corretores_db";
 
+
 // Cria a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -12,4 +13,5 @@ if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
 echo "Conectado com sucesso";
+
 ?>
